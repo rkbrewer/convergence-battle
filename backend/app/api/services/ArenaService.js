c108fde43c89ec0b053d2a67169e99ff6e63ae42
Ryan Brewer <webbrewer@gmail.com>
2017-03-02 23:39:18 -0600

    notes

--- backend/app/api/services/ArenaService.js
+++ backend/app/api/services/ArenaService.js
@@ -1,27 +1,16 @@
 /*
- Multiplayer:
- 0. A player pushes Start on Client (sends signal to webserver, joining them to a lobby)
- 1. A player waits in a lobby (in debug/admin mode I list all players in the lobby)
- 2. Once 2 players join, the server-app selects 2 players and creates an arena-engine instance
- 3. Battle starts, input is relayed to server over websockets...?
-
- I'll have to test this.  Phase 1, get a single player, local engine to work.
- Phase 2, put the engine on a webserver and update bodies over websockets.
 
  Firebase will be my db. My frontend/backend can both talk to it.
  Something else will host my node app.  now? Yes, use Now.
- My server app should be ... express + express-ws (websockets). No just try sails.
-
-
-
 
 
  Multiplayer:
  Server gets 2 people in a lobby, connected via websockets, then broadcasts a signal to each of them
- telling them to start-your-engines.
+ telling them to start-your-engines. In this approach the server is never running a simulation, rather it
+ acts as a message bus between client-simulations, signaling simulations to start/stop and each player's input.
 
- Each client has a websocket connection to each players ships, and the server relays messages back and forth.
- Each players ship has an id: ship1, ship2.  The server sends messages back and forth.
+ Each client has a websocket connection to arena inputs, and the server relays messages back and forth.
+ Each players ship has an id: playerOneUid, playerTwoUid.
 
  example:
  /input sends {
